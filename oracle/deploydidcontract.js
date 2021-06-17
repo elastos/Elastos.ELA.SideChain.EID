82494058fa00d9d4480581f4ac7448af46583351
ttblack <zhangxiaobin@elastos.org>
2021-06-18 00:45:37 +0800

    modify port

--- oracle/deploydidcontract.js
+++ oracle/deploydidcontract.js
@@ -1,8 +1,7 @@
 "use strict";
 
 const Web3 = require("web3");
-// const web3 = new Web3("http://52.80.107.251:1111");
-const web3 = new Web3("http://127.0.0.1:1111");
+const web3 = new Web3("http://127.0.0.1:20646");
 
 const acc = web3.eth.accounts.decrypt({"address":"53781e106a2e3378083bdcede1874e5c2a7225f8","crypto":{"cipher":"aes-128-ctr","ciphertext":"bc53c1fcd6e31a6392ddc1777157ae961e636c202ed60fb5dda77244c5c4b6ff","cipherparams":{"iv":"c5d1a7d86d0685aa4542d58c27ae7eb4"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"409429444dabb5664ba1314c93f0e1d7a1e994a307e7b43d3f6cc95850fbfa9f"},"mac":"4c37821c90d35118182c2d4a51356186482662bb945f0fcd33d3836749fe59c0"},"id":"39e7770e-4bc6-42f3-aa6a-c0ae7756b607","version":3}, "123");  
 
