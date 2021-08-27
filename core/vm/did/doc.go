265ca9033d95f3c734ec34c3702140c66a4e8ffd
jiangzehua <1092431698@qq.com>
2021-08-27 15:45:12 +0800

    add Mashal function for DID payload

--- core/vm/did/doc.go
+++ core/vm/did/doc.go
@@ -1,8 +1,12 @@
 package did
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
+	"sort"
 	"strings"
 
 	"github.com/elastos/Elastos.ELA.SideChain.EID/core/vm/did/didjson"
@@ -73,6 +77,203 @@ func (p *VerifiableCredentialData) GetData() []byte {
 	return data
 }
 
+func MarshalVerifiableCredentialData(p *VerifiableCredentialData, buf *bytes.Buffer) error {
+	buf.WriteRune('{')
+
+	// ID
+	err := writeKey(buf, "id")
+	if err != nil {
+		return err
+	}
+	idv, err := json.Marshal(p.ID)
+	if err != nil {
+		return err
+	}
+	buf.Write(idv)
+	buf.WriteRune(',')
+
+	// Type
+	count := len(p.Type)
+	if count != 0 {
+		err := writeKey(buf, "type")
+		if err != nil {
+			return err
+		}
+		tpe, err := json.Marshal(p.Type)
+		if err != nil {
+			return err
+		}
+		buf.Write(tpe)
+		buf.WriteRune(',')
+	}
+
+	// Issuer
+	if err = writeKey(buf, "issuer"); err != nil {
+		return err
+	}
+	ise, err := json.Marshal(p.Issuer)
+	if err != nil {
+		return err
+	}
+	buf.Write(ise)
+	buf.WriteRune(',')
+
+	// IssuanceDate
+	err = writeKey(buf, "issuanceDate")
+	if err != nil {
+		return err
+	}
+	isd, err := json.Marshal(p.Issuer)
+	if err != nil {
+		return err
+	}
+	buf.Write(isd)
+	buf.WriteRune(',')
+
+	// ExpirationDate
+	err = writeKey(buf, "expirationDate")
+	if err != nil {
+		return err
+	}
+	exp, err := json.Marshal(p.Issuer)
+	if err != nil {
+		return err
+	}
+	buf.Write(exp)
+	buf.WriteRune(',')
+
+	// CredentialSubject
+	err = writeKey(buf, "credentialSubject")
+	if err != nil {
+		return err
+	}
+	err = MarshalCredentialSubject(p.CredentialSubject, buf)
+	if err != nil {
+		return err
+	}
+
+	buf.WriteRune('}')
+	fmt.Println(buf.String())
+	return nil
+}
+
+func MarshalCredentialSubject(credentialSubject interface{}, buf *bytes.Buffer) error {
+	creSub := credentialSubject.(map[string]interface{})
+	_, ok := creSub["id"]
+	if !ok {
+		return errors.New("not found id in credential kvData")
+	}
+
+	type subject struct {
+		key   string
+		value interface{}
+	}
+	sortedData := make([]subject, 0)
+	for k, v := range creSub {
+		sortedData = append(sortedData, subject{k, v})
+	}
+
+	sort.Slice(sortedData, func(i, j int) bool {
+		if sortedData[i].key == "id" {
+			return true
+		}
+		if sortedData[j].key == "id" {
+			return false
+		}
+
+		return sortedData[i].key < sortedData[j].key
+	})
+
+	l := len(sortedData)
+	for i, data := range sortedData {
+		buf.WriteRune('{')
+		err := writeKey(buf, data.key)
+		if err != nil {
+			return err
+		}
+		idv, err := json.Marshal(data.value)
+		if err != nil {
+			return err
+		}
+		buf.Write(idv)
+
+		if i != l-1 {
+			buf.WriteRune(',')
+		} else {
+			buf.WriteRune('}')
+		}
+	}
+
+	return nil
+}
+
+func MarshalService(service interface{}, buf *bytes.Buffer) error {
+	ser := service.(map[string]interface{})
+	if _, ok := ser["id"]; !ok {
+		return errors.New("not found id in service")
+	}
+	if _, ok := ser["type"]; !ok {
+		return errors.New("not found type in service")
+	}
+	if _, ok := ser["serviceEndpoint"]; !ok {
+		return errors.New("not found serviceEndpoint in service")
+	}
+
+	type kvData struct {
+		key   string
+		value interface{}
+	}
+	sortedData := make([]kvData, 0)
+	for k, v := range ser {
+		sortedData = append(sortedData, kvData{k, v})
+	}
+
+	sort.Slice(sortedData, func(i, j int) bool {
+		if sortedData[i].key == "id" {
+			return true
+		}
+		if sortedData[j].key == "id" {
+			return false
+		}
+		if sortedData[i].key == "type" {
+			return true
+		}
+		if sortedData[j].key == "type" {
+			return false
+		}
+		if sortedData[i].key == "serviceEndpoint" {
+			return true
+		}
+		if sortedData[j].key == "serviceEndpoint" {
+			return false
+		}
+
+		return sortedData[i].key < sortedData[j].key
+	})
+
+	l := len(sortedData)
+	for i, data := range sortedData {
+		buf.WriteRune('{')
+		err := writeKey(buf, data.key)
+		if err != nil {
+			return err
+		}
+		idv, err := json.Marshal(data.value)
+		if err != nil {
+			return err
+		}
+		buf.Write(idv)
+
+		if i != l-1 {
+			buf.WriteRune(',')
+		} else {
+			buf.WriteRune('}')
+		}
+	}
+
+	return nil
+}
+
 type Service struct {
 	ID              string `json:"id"`
 	Type            string `json:"type"`
@@ -89,16 +290,16 @@ type DIDPayloadData struct {
 	Authentication       []interface{}          `json:"authentication,omitempty"`
 	Authorization        []interface{}          `json:"authorization,omitempty"`
 	VerifiableCredential []VerifiableCredential `json:"verifiableCredential,omitempty"`
-	Service              []Service              `json:"service,omitempty"`
+	Service              []interface{}          `json:"service,omitempty"`
 	Expires              string                 `json:"expires"`
 }
 
-type PublicKeysSlice [] DIDPublicKeyInfo
+type PublicKeysSlice []DIDPublicKeyInfo
 
 func (a PublicKeysSlice) Len() int {
 	return len(a)
 }
-func (a PublicKeysSlice) Swap(i, j int){
+func (a PublicKeysSlice) Swap(i, j int) {
 	a[i], a[j] = a[j], a[i]
 }
 func (a PublicKeysSlice) Less(i, j int) bool {
@@ -109,13 +310,12 @@ func (a PublicKeysSlice) Less(i, j int) bool {
 	return result
 }
 
-
-type ServiceSlice [] Service
+type ServiceSlice []Service
 
 func (s ServiceSlice) Len() int {
 	return len(s)
 }
-func (s ServiceSlice) Swap(i, j int){
+func (s ServiceSlice) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 func (s ServiceSlice) Less(i, j int) bool {
@@ -126,12 +326,12 @@ func (s ServiceSlice) Less(i, j int) bool {
 	return result
 }
 
-type VerifiableCredentialSlice [] VerifiableCredential
+type VerifiableCredentialSlice []VerifiableCredential
 
 func (v VerifiableCredentialSlice) Len() int {
 	return len(v)
 }
-func (v VerifiableCredentialSlice) Swap(i, j int){
+func (v VerifiableCredentialSlice) Swap(i, j int) {
 	v[i], v[j] = v[j], v[i]
 }
 func (v VerifiableCredentialSlice) Less(i, j int) bool {
@@ -142,7 +342,161 @@ func (v VerifiableCredentialSlice) Less(i, j int) bool {
 	return result
 }
 
+func (p *DIDPayloadData) MarshalJSON() ([]byte, error) {
+	var b []byte
+	buf := bytes.NewBuffer(b)
+	buf.WriteRune('{')
+
+	// ID
+	err := writeKey(buf, "id")
+	if err != nil {
+		return nil, err
+	}
+	idv, err := json.Marshal(p.ID)
+	if err != nil {
+		return nil, err
+	}
+	buf.Write(idv)
+
+	// Controller
+	if p.Controller != nil {
+		err := writeKey(buf, "controller")
+		if err != nil {
+			return nil, err
+		}
+
+		//if id, ok := p.Controller.(string); ok {
+		idv, err := json.Marshal(p.Controller)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(idv)
+		//}
+
+		buf.WriteRune(',')
+	}
+
+	// MultiSig
+	if p.MultiSig != "" {
+		err := writeKey(buf, "multisig")
+		if err != nil {
+			return nil, err
+		}
+		sigv, err := json.Marshal(p.MultiSig)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(sigv)
+		buf.WriteRune(',')
+	}
+
+	// Publickey
+	count := len(p.PublicKey)
+	if count != 0 {
+		err := writeKey(buf, "publicKey")
+		if err != nil {
+			return nil, err
+		}
+		pks, err := json.Marshal(p.PublicKey)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(pks)
+		buf.WriteRune(',')
+	}
+
+	// Authentication
+	count = len(p.Authentication)
+	if count != 0 {
+		err := writeKey(buf, "authentication")
+		if err != nil {
+			return nil, err
+		}
+		ath, err := json.Marshal(p.Authentication)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(ath)
+		buf.WriteRune(',')
+	}
+
+	// Authorization
+	count = len(p.Authorization)
+	if count != 0 {
+		err := writeKey(buf, "authorization")
+		if err != nil {
+			return nil, err
+		}
+		ath, err := json.Marshal(p.Authorization)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(ath)
+		buf.WriteRune(',')
+	}
+
+	// VerifiableCredential
+	count = len(p.VerifiableCredential)
+	if count != 0 {
+		err := writeKey(buf, "verifiableCredential")
+		if err != nil {
+			return nil, err
+		}
+		buf.WriteRune('[')
+		for i, vc := range p.VerifiableCredential {
+			if err = MarshalCredentialSubject(vc, buf); err != nil {
+				return nil, err
+			}
+			if i != count-1 {
+				buf.WriteRune(',')
+			}
+		}
+		buf.WriteRune(']')
+		buf.WriteRune(',')
+	}
+
+	// Servie
+	count = len(p.Service)
+	if count != 0 {
+		if err := writeKey(buf, "service"); err != nil {
+			return nil, err
+		}
+		if err := MarshalService(p.Service, buf); err != nil {
+			return nil, err
+		}
+		buf.WriteRune(',')
+	}
+
+	// Expires
+	if p.Expires != "" {
+		if err := writeKey(buf, "expires"); err != nil {
+			return nil, err
+		}
+		sigv, err := json.Marshal(p.Expires)
+		if err != nil {
+			return nil, err
+		}
+		buf.Write(sigv)
+	}
+
+	buf.WriteRune('}')
+
+	fmt.Println(buf.String())
+	return buf.Bytes(), nil
+}
+
+func writeKey(buf *bytes.Buffer, key string) error {
+	sig, err := json.Marshal(key)
+	if err != nil {
+		return err
+	}
+	buf.Write(sig)
+	buf.WriteRune(':')
+	return nil
+}
+
 func (c *DIDPayloadData) GetData() []byte {
+
 	data, err := didjson.Marshal(c)
 	if err != nil {
 		return nil
@@ -157,7 +511,6 @@ type DIDPublicKeyInfo struct {
 	Controller      string `json:"controller"`
 	PublicKeyBase58 string `json:"publicKeyBase58"`
 }
-
 
 func (p *DIDPublicKeyInfo) Serialize(w io.Writer, version byte) error {
 	if err := common.WriteVarString(w, p.ID); err != nil {
@@ -204,13 +557,9 @@ func (p *DIDPublicKeyInfo) Deserialize(r io.Reader, version byte) error {
 	return nil
 }
 
-
 func IsCompact(target string) bool {
 	if !strings.HasPrefix(target, DID_ELASTOS_PREFIX) {
 		return true
 	}
 	return false
 }
-
-
-
