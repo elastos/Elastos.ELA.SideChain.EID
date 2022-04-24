db71e56a4a2469fff1888d2f7f031ae79107f15c
jiangzehua <1092431698@qq.com>
2022-04-24 15:43:15 +0800

    feat(nfc): use NFC instead of NFKD

---
+++ core/vm/did/nfcbuffer.go
@@ -0,0 +1,46 @@
+package did
+
+import (
+	"bytes"
+	"encoding/json"
+	"golang.org/x/text/unicode/norm"
+)
+
+type NFCBuffer struct {
+	*bytes.Buffer
+}
+
+func (buf *NFCBuffer) Write(b []byte) (n int, err error) {
+	return buf.Buffer.Write(ToNFCBytes(b))
+}
+
+func (buf *NFCBuffer) WriteString(s string) (n int, err error) {
+	return buf.Buffer.WriteString(ToNFCString(s))
+}
+
+func (buf *NFCBuffer) WriteKey(key string) error {
+	sig, err := json.Marshal(key)
+	if err != nil {
+		return err
+	}
+	if _, err := buf.Write(sig); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(":"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ToNFCString(s string) string {
+	return norm.NFC.String(s)
+}
+
+func ToNFCBytes(b []byte) []byte {
+	return norm.NFC.Bytes(b)
+}
+
+func NewNFCBuffer() *NFCBuffer {
+	return &NFCBuffer{new(bytes.Buffer)}
+}
