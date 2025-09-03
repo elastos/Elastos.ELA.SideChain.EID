0e35d3b466f6b32f35840e7439cb9371f17b25bf
vito <zenith@elascript.net>
2025-09-03 22:18:50 +0800

    change version to v0.2.3.2

--- params/version.go
+++ params/version.go
@@ -26,7 +26,7 @@ const (
 	VersionPatch = 7 // Patch version component of the current release
 	VersionCross = 0 // Cross version component of the current release
 	VersionEVM   = "v1.12.0"
-	VersionMeta  = "v0.2.3.1" // Version metadata to append to the version string
+	VersionMeta  = "v0.2.3.2" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
